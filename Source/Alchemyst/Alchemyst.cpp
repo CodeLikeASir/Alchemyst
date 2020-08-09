9f09e5c205aace43300a39cfa871843d39ca306f
Jan Meissner <meissnej@hochschule-trier.de>
2020-08-09 23:02:00 +0200

    Initial commit

---
+++ Source/Alchemyst/Alchemyst.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Alchemyst.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Alchemyst, "Alchemyst" );
+
+DEFINE_LOG_CATEGORY(LogAlchemyst)
+ 
